68672a10cc01c5f0c50aaa5779eab8af33046f43
Adam NAILI <a.naili@live.fr>
2017-12-06 21:02:50 +0100

    Update doc on button.h

--- include/gui/button.h
+++ include/gui/button.h
@@ -3,14 +3,34 @@
 
 /**
  * File: button.h
+ * Buttons handling
  */
 
+/**
+ * Type: Button
+ * Component that can be triggered by click to execute a specific action.
+ */
 typedef struct {
   Component component;
 } Button;
 
+/**
+ * Function: button_init
+ * Initializes the button.
+ *
+ * Parameters:
+ *  *button - pointer to the input button
+ *  text - label for the button
+ */
 void button_init(Button *button, char *text);
 
+/**
+ * Function: button_free
+ * Frees the resources for the button.
+ *
+ * Parameters:
+ *  *button - pointer to the input button
+ */
 void button_free(Button *button);
 
 #endif

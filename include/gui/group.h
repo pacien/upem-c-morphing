c9f0cce6b9e6c50fdda1cb94a6ac1831264ab020
Adam NAILI <a.naili@live.fr>
2017-12-09 02:37:14 +0100

    Rework of the structure Group

--- include/gui/group.h
+++ include/gui/group.h
@@ -1,13 +1,20 @@
 #ifndef UPEM_MORPHING_GROUP
 #define UPEM_MORPHING_GROUP
 
+#include "window.h"
+
 /**
  * File: group.h
  */
 
+typedef struct _GroupElement {
+  Component *c;
+  struct _GroupElement *next;
+} GroupElement;
+
 typedef struct {
   Component component;
-  Component *sub_components;
+  GroupElement *group_head;
 } Group;
 
 void group_init(Group *group, int padding);

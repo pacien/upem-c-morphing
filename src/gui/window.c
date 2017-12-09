a7e6f8a296b536966cee49f8de105960970d04c5
Adam NAILI <a.naili@live.fr>
2017-12-09 01:58:10 +0100

    Creating source file window.c

---
+++ src/gui/window.c
@@ -0,0 +1,13 @@
+#include "gui/window.h"
+
+void window_init(Window *window, int width, int height, char *title) {
+//TODO
+}
+
+void window_free(Window *window) {
+//TODO
+}
+
+void window_add_component(Window *window, Component *component) {
+//TODO
+}

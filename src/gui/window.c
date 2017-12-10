2aed52d89856847ea394b06d7b70782fa6b21d24
Adam NAILI <a.naili@live.fr>
2017-12-10 13:01:42 +0100

    Implementing init, free, add_button, add_pictureframe functions for window

--- src/gui/window.c
+++ src/gui/window.c
@@ -1,13 +1,30 @@
-#include "gui/window.h"
+#include <stdlib.h>
+#include <gui/window.h>
+#include <common/mem.h>
+#include "string.h"
+#include "assert.h"
 
 void window_init(Window *window, int width, int height, char *title) {
-//TODO
+  window = malloc_or_die(sizeof(window));
+  assert(width > 0);
+  assert(height > 0);
+  window->width = width;
+  window->height = height;
+  assert(title != NULL);
+  strcpy(window->title,title);
+  window->group_buttons = malloc_or_die(sizeof(Group));
+  window->group_pictureframe = malloc_or_die(sizeof(Group));
 }
 
 void window_free(Window *window) {
-//TODO
+  group_free(window->group_buttons);
+  group_free(window->group_pictureframe);
 }
 
-void window_add_component(Window *window, Component *component) {
-//TODO
+void window_add_button(Window *window, Component *component){
+  group_add_component(window->group_buttons,component);
+}
+
+void window_add_pictureframe(Window *window, Component *component){
+  group_add_component(window->group_pictureframe,component);
 }

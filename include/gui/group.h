3449e695fa8885bb8150e105c267d92ab8a3d4ee
Adam NAILI <a.naili@live.fr>
2017-12-10 13:28:40 +0100

    Updating documentation of group.h

--- include/gui/group.h
+++ include/gui/group.h
@@ -1,17 +1,35 @@
 #ifndef UPEM_MORPHING_GROUP
 #define UPEM_MORPHING_GROUP
 
+/**
+ * File: group.h
+ * Group of components
+ */
 #include "window.h"
 
 /**
- * File: group.h
+ * Struct: _GroupElement
+ * Nod of the linked list that is involved in the Group behavior
+ *
+ * Parameters:
+ *  *component - component
+ *  *sub_component - sub component
+ *  *next - link to the next nod
  */
-
 typedef struct _GroupElement {
-  Component *c;
+  Component *component;
+  Component *sub_component;
   struct _GroupElement *next;
 } GroupElement;
 
+/**
+ * Struct: Group
+ * Group representation
+ *
+ * Parameters:
+ *  component - Component used for the Group to catch clicks and handle print to delegate to the components contented in it
+ *  *group_head - pointer to the head of the list that regroup all the components contained inside of the group
+ */
 typedef struct {
   Component component;
   GroupElement *group_head;

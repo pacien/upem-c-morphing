6802eedbdde0e5a42bb7b6fc251b3123c295666f
pacien <pacien.trangirard@pacien.net>
2017-12-22 13:50:34 +0100

    Rename function accurately
    Signed-off-by: pacien <pacien.trangirard@pacien.net>

--- src/morpher/quadrilateral.c
+++ src/morpher/quadrilateral.c
@@ -6,7 +6,7 @@ static inline IntVector p2(IntVector n) {
   return n * n;
 }
 
-static inline bool in_circumcircle(TriangleMap *t, CartesianVector v) {
+static inline bool not_in_circumcircle(TriangleMap *t, CartesianVector v) {
   CartesianVector a = t->vertices[0].origin, b = t->vertices[1].origin, c = t->vertices[2].origin;
   IntVector v2 = p2(v.x) + p2(v.y);
   return matrix_int_det3(a.x - v.x, a.y - v.y, p2(a.x) + p2(a.y) - v2,
@@ -53,6 +53,6 @@ void quadrilateral_flip_diagonal(TriangleMap *t1, TriangleMap *t2) {
 }
 
 bool quadrilateral_is_delaunay(TriangleMap *t1, TriangleMap *t2) {
-  return in_circumcircle(t1, t2->vertices[(trianglemap_find_common_edge(t2, t1) + 2) % 3].origin) &&
-         in_circumcircle(t2, t1->vertices[(trianglemap_find_common_edge(t1, t2) + 2) % 3].origin);
+  return not_in_circumcircle(t1, t2->vertices[(trianglemap_find_common_edge(t2, t1) + 2) % 3].origin) &&
+         not_in_circumcircle(t2, t1->vertices[(trianglemap_find_common_edge(t1, t2) + 2) % 3].origin);
 }

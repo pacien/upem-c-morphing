190449ee18bec69b2e385dccd9bd42ddc83dd418
pacien <pacien.trangirard@pacien.net>
2017-12-28 01:22:41 +0100

    Refactor and test color
    Signed-off-by: pacien <pacien.trangirard@pacien.net>

---
+++ include/painter/color.h
@@ -0,0 +1,61 @@
+#ifndef UPEM_MORPHING_COLOR
+#define UPEM_MORPHING_COLOR
+
+/**
+ * File: color.h
+ *
+ * See also:
+ *   A rainbow
+ */
+
+#include <MLV/MLV_color.h>
+#include <stdbool.h>
+#include "common/time.h"
+
+/**
+ * Type: ColorComponent
+ * Represents a single colour component of 32-bits RGBa tuple.
+ */
+typedef uint8_t ColorComponent;
+
+/**
+ * Type: ColorPixel
+ * Represents a single RGBa coloured pixel.
+ * Compatible with the libMLV representation.
+ */
+typedef union {
+  struct {
+    ColorComponent a, b, g, r;
+  } rgba;
+
+  MLV_Color mlv;
+} Color;
+
+/**
+ * Function: color_equals
+ * Compares the supplied colors.
+ *
+ * Parameters:
+ *   c1 - the first color
+ *   c2 - the second color
+ *
+ * Returns:
+ *   T(c1 is the same color as c2)
+ */
+bool color_equals(Color c1, Color c2);
+
+/**
+ * Function: color_blend
+ * Blends two colors.
+ *
+ * Parameters:
+ *   origin   - the first color
+ *   target   - the second color
+ *   distance - the distance from the first color
+ *
+ * Returns:
+ *   The blended color
+ */
+Color color_blend(Color origin, Color target, TimeVector distance);
+
+#endif
--- src/blender/color.c
+++
@@ -1,8 +0,0 @@
-#include "blender/color.h"
-
-bool color_equals(Color c1, Color c2) {
-  return c1.rgba.r == c2.rgba.r &&
-         c1.rgba.g == c2.rgba.g &&
-         c1.rgba.b == c2.rgba.b &&
-         c1.rgba.a == c2.rgba.a;
-}

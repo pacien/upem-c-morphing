330fd85db8c89c178621d978929d911bbe93fec7
pacien <pacien.trangirard@pacien.net>
2017-12-28 01:23:08 +0100

    Refactor canvas blender into rasterizer
    Signed-off-by: pacien <pacien.trangirard@pacien.net>

---
+++ include/painter/rasterizer.h
@@ -0,0 +1,41 @@
+#ifndef UPEM_MORPHING_RASTERIZER
+#define UPEM_MORPHING_RASTERIZER
+
+/**
+ * File: rasterizer.h
+ */
+
+#include "painter/canvas.h"
+#include "morpher/morphing.h"
+
+/**
+ * Struct: RasterizationContext
+ */
+typedef struct {
+  Canvas *result, *source, *target;
+  TimeVector frame;
+} RasterizationContext;
+
+/**
+ * Struct: TriangleContext
+ */
+typedef struct {
+  Triangle current, source, target;
+} TriangleContext;
+
+/**
+ * Function: rasterize
+ * Rasterises a morphing from a source and a target image at the given time frame.
+ *
+ * Parameters:
+ *   *source - source image canvas
+ *   *target - target image canvas
+ *   *m      - reference morphing
+ *   frame   - time frame
+ *
+ * Returns:
+ *   The drawn canvas, dynamically allocated
+ */
+Canvas *rasterize(Canvas *source, Canvas *target, Morphing *m, TimeVector frame);
+
+#endif

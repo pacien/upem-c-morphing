ec4161223661a4b3d01719dc40eaa85c1dce15d7
pacien <pacien.trangirard@pacien.net>
2018-01-09 15:29:29 +0100

    Reformat and clean includes
    Signed-off-by: pacien <pacien.trangirard@pacien.net>

--- src/main.c
+++ src/main.c
@@ -1,16 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <getopt.h>
+#include <MLV/MLV_path.h>
 #include "gui/gui.h"
-#include "MLV/MLV_path.h"
 
 int main(int argc, char **argv) {
   int opt = 0;
   int help = -1;
   static struct option long_options[] = {
-    {"help", 0, 0, 'h'},
-    {0, 0, 0, 0}
+   {"help", 0, 0, 'h'},
+   {0,      0, 0, 0}
   };
   int long_index = 0;
   /*Option handling*/
@@ -25,7 +24,7 @@ int main(int argc, char **argv) {
   }
   if (help == 0) {
     printf(
-      "--Morphing's Help--\nTo use the morphing, you need to put two correct paths to image files.\n\nCorrect image format :\nICO(Icon)/CUR(Cursor)/BMP, PNM (PPM/PGM/PBM), XPM, LBM(IFF ILBM), PCX, GIF, JPEG, PNG, TGA, TIFF, and XV.\n\nMorphing made by:\n Pacien TRAN-GIRARD and Adam NAILI\n");
+     "--Morphing's Help--\nTo use the morphing, you need to put two correct paths to image files.\n\nCorrect image format :\nICO(Icon)/CUR(Cursor)/BMP, PNM (PPM/PGM/PBM), XPM, LBM(IFF ILBM), PCX, GIF, JPEG, PNG, TGA, TIFF, and XV.\n\nMorphing made by:\n Pacien TRAN-GIRARD and Adam NAILI\n");
   } else {
     int nbArg = argc - optind;
     if (nbArg < 2) {
